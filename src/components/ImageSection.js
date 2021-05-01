3ad656a7e7586af2844b753d4ea5a6c3add2be19
akash <akash.kushwaha02@gmail.com>
2021-05-01 14:48:35 +0530

    added skillSection

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -1,25 +1,40 @@
-import React from 'react'
-import about from '../img/about.jpg'
+import React from "react";
+import about from "../img/about.jpg";
 
-function ImageSection () {
-    return (
-        <div className="ImageSection">
-                <div className="img">
-                    <img src={about}  alt="" />
-                </div>
-                <div className="about-info" >
-                    <h4>I am <span> Akash Singh Kushwaha</span></h4>
-                    <p>
-                        lorem ipsumlorem ipsumlorem ipsumlorem ipsum
-                        lorem ipsumlorem ipsum
-                        lorem ipsumlorem ipsum
-                    </p>
-                    <div className="about-details">
-
-                    </div>
-                </div>
+function ImageSection() {
+  return (
+    <div className="ImageSection">
+      <div className="img">
+        <img src={about} alt="" />
+      </div>
+      <div className="about-info">
+        <h4>
+          I am <span> IRONMAN</span>
+        </h4>
+        <p className="about-text">
+          lorem ipsumlorem ipsumlorem ipsumlorem ipsum lorem ipsumlorem ipsum
+          lorem ipsumlorem ipsu
+        </p>
+        <div className="about-details">
+          <div className="left-section">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Address</p>
+            <p>Nationality</p>
+            <p>Languages</p>
+          </div>
+          <div className="right-section">
+            <p className="">: AKASH SINGH KUSHWAHA </p>
+            <p className="">: 23</p>
+            <p className="">: Jabalpur</p>
+            <p className="">: Indian</p>
+            <p className="">: Hindi, English</p>
+          </div>
         </div>
-    )
+        <button className="btn">Download CV</button>
+      </div>
+    </div>
+  );
 }
 
 export default ImageSection;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import ImageSection from '../components/ImageSection'
-import Title from '../components/Title'
+import React from "react";
+import ImageSection from "../components/ImageSection";
+import SkillSection from "../components/SkillSection";
+import Title from "../components/Title";
 
-function About () {
-    return(
-        <div className="About">
-            <Title title={'About me'} span={'About me span'} />
-            <ImageSection />
-        </div>
-    )
+function About() {
+  return (
+    <div className="About">
+      <Title title={"About me"} span={"About me"} />
+      <ImageSection />
+      <Title title={"My Skills"} span={"My Skills"} />
+      <div className="about-skills-container">
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+        <SkillSection skill={"Javascript"} progress={"50%"} width={"50%"} />
+      </div>
+    </div>
+  );
 }
 
 export default About;
